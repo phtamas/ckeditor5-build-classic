90d6544bd9683e96d9073f08b3542c428764890a
phtamas <phtamas@gmail.com>
2018-10-30 18:21:55 +0100

    Removed @ckeditor/ckeditor5-adapter-ckfinder and @ckeditor/ckeditor5-easy-image plugins

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -7,12 +7,10 @@
 import ClassicEditorBase from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
 
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
-//import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
-//import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading';
 import Image from '@ckeditor/ckeditor5-image/src/image';
 import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
@@ -32,12 +30,10 @@ export default class ClassicEditor extends ClassicEditorBase {}
 // Plugins to include in the build.
 ClassicEditor.builtinPlugins = [
 	Essentials,
-	//UploadAdapter,
 	Autoformat,
 	Bold,
 	Italic,
 	BlockQuote,
-	//EasyImage,
 	Heading,
 	Image,
 	ImageCaption,
